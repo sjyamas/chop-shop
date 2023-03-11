ee22f3a182c482a97e96c014873a9165ab142f9f
Shohei Y <64562970+sjyamas@users.noreply.github.com>
2023-03-11 05:08:58 -0800

    message on front page

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -2,18 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CustomButton from "../Global/CustomButton.jsx";
 
+import bjchart from "../Assets/Images/bjchart.png";
+
 export default function Pages() {
   return (
     <div>
       <div className="App-header">
-        <CustomButton>
-          <Link to="chop-shop" style={{ color: "white" }}>
-            Chop Shop
-          </Link>
-        </CustomButton>
-        <CustomButton>
-          <Link to="Blackjack-Log" style={{ color: "white" }}> Blackjack Log </Link>
-        </CustomButton>
+        <div style={{ display: "flex" }}>
+          <div>
+            <CustomButton>
+              <Link to="chop-shop" style={{ color: "white" }}>
+                <h1> Chop Shop </h1>
+              </Link>
+            </CustomButton>
+            <CustomButton>
+              <Link to="Blackjack-Log" style={{ color: "white" }}>
+                <h1> [NEW] Blackjack Log</h1>
+              </Link>
+            </CustomButton>
+          </div>
+          <div>
+            <h2 style={{ margin: 0 }}>
+              {" "}
+              FOR THE LOVE OF GOD DYLAM USE THE CHART &emsp;{" "}
+            </h2>
+            <h3> and no im not delusional. the wheel doesn't like me </h3>
+          </div>
+          <img src={bjchart} alt="bjchart" style={{ width: "30%" }} />
+        </div>
       </div>
     </div>
   );
