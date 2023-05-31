e1b47c449144a983e9e6c1d499ab7985023a0b24
Shohei Y <64562970+sjyamas@users.noreply.github.com>
2023-05-30 18:12:45 -0700

    media quesries

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -4,16 +4,20 @@ import CustomButton from "../Global/CustomButton.jsx";
 
 import bjchart from "../Assets/Images/bjchart.png";
 
+import './Pages.css'
+
 export default function Pages() {
   return (
-      <div className="App-header" style={{display: 'flex', flexDirection: 'column'}}>
-        <div style ={{display: "flex", justifyContent: "center"}}>
+      <div className="App-header front-body">
+        <div className="front-title">
           <h1 style={{textAlign: 'center'}}>Boofman's website for all yotti things</h1>
         
         </div>
-        <hr style={{width: "90%"}}/>
-        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-          <div style={{ display: "flex", flexDirection: 'column', padding: 5}}>
+        <div style={{padding: 20}}>
+          <hr style={{width: "90%", }}/>
+        </div>
+        <div className="front-body">
+          <div className="front-col">
             <CustomButton>
               <Link to="chop-shop" style={{ color: "white" }}>
                 <h1> Chop Shop </h1>
@@ -26,14 +30,14 @@ export default function Pages() {
             </CustomButton>
             
           </div>
-          <div style={{ display: "flex", flexDirection: 'column', padding: 5}}>
+          <div className="front-col">
             <div>
               <h2 style={{ margin: 0, }}>
                 MOTD:
               </h2>
               <h3 style={{ margin: 0, paddingBottom: 50}}> you look mighty fine </h3>
             </div>   
-            <div style={{display: "inline-block", overflow: "break-word", inlineSize:"372px"}}>
+            <div className="front-col-text">
               <h2 style={{ margin: 0 }}>BlackJack Chart:</h2>
               <h3 style={{ margin: 0 }}>Failure to use chart will result in suboptimal results</h3>
 
